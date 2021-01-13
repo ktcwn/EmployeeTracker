895645be7600a01910ccb182b21308eaf2fb0354
Katelynn Cowan <kcowan@twu.edu>
2021-01-12 20:30:11 -0600

    created seeds and schema SQL pages and began writing code for JavaScript to allow express, SQL and handlebars.

---
+++ server.js
@@ -0,0 +1,32 @@
+let express = require("express");
+let exphbs = require("express-handlebars");
+let mysql = require("mysql");
+
+let app = express();
+
+let PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employees_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
